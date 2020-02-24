6a69520623a4aaa0e7e91f6dfa35efb7555c5ddd
tboutin <tboutin@ippon.fr>
2020-02-24 16:17:52 +0100

    [FEATURE] iOS kotlin multiplatform project + update .gitignore

--- SharedCode/src/iosMain/kotlin/actual.kt
+++ SharedCode/src/iosMain/kotlin/actual.kt
@@ -1,14 +1,9 @@
 package fr.ippon.myapplicationkn
-/*
+
 import platform.UIKit.UIDevice
 
 actual fun platformName(): String {
     return UIDevice.currentDevice.systemName() +
             " " +
             UIDevice.currentDevice.systemVersion
-}
- */
-
-actual fun platformName(): String {
-    return "iOS"
 }
